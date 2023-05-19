69b3682a6958dc33f13c7748287695f311110547
jonah wagner <jonahwagner231@gmail.com>
2023-05-18 23:21:01 -0700

    added ability to close dropdown by clicking anywhere else on the page

--- components/dropdownToggle.tsx
+++ components/dropdownToggle.tsx
@@ -1,18 +1,29 @@
 "use client";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 
 import SecondaryButton from "./secondaryButton";
 
 export default function DropdownToggle() {
 	const [expanded, setExpanded] = useState(false);
+	const dropdownButton = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		window.addEventListener("click", (event: Event) => {
+			if (
+				dropdownButton.current &&
+				dropdownButton.current.contains(event.target as Node)
+			) {
+				setExpanded(!expanded);
+			} else {
+				setExpanded(false);
+			}
+		});
+	});
 
 	return (
-		<>
-			<GiHamburgerMenu
-				onClick={() => setExpanded(!expanded)}
-				className="sm:hidden text-3xl text-white border-1 border-white hover:-translate-y-1 hover:shadow-secondary"
-			/>
+		<div ref={dropdownButton}>
+			<GiHamburgerMenu className="sm:hidden text-3xl text-white border-1 border-white hover:-translate-y-1 hover:shadow-secondary" />
 			{expanded && (
 				<div className="sm:hidden absolute top-20 right-0 w-1/2 h-40 bg-background opacity-90 rounded">
 					<ul className="flex flex-col justify-center items-center h-full gap-2 p-3">
@@ -29,6 +40,6 @@ export default function DropdownToggle() {
 					</ul>
 				</div>
 			)}
-		</>
+		</div>
 	);
 }
