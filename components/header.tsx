2ea1a724fd83412914d0b11433d40b3eb907557b
jonah wagner <jonahwagner231@gmail.com>
2023-05-27 12:49:00 -0700

    refactored to render Link for nav or button for events

--- components/header.tsx
+++ components/header.tsx
@@ -4,12 +4,20 @@ import PrimaryButton from "./primaryButton";
 import SecondaryButton from "./secondaryButton";
 import DropDownToggle from "./dropdownToggle";
 
-export default function Header() {
+import { signIn, signOut } from "next-auth/react";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+
+export default async function Header() {
+	const session = await getServerSession(authOptions);
 	return (
 		<header className="z-10 fixed top-0 left-0 w-full h-20 bg-background">
 			<nav className="h-full flex">
 				<ul className="w-1/2 flex justify-start gap-2 items-center h-full p-3">
-					<PrimaryButton label="Login" link="" />
+					<PrimaryButton
+						label={session ? "Logout" : "Login"}
+						link=""
+					/>
 					<Image
 						className="rounded-full border-2 border-accent-dark justify-self-end hidden sm:inline"
 						src="/logo.png"
--- components/primaryButton.tsx
+++ components/primaryButton.tsx
@@ -1,38 +1,44 @@
 "use client";
+import { signIn, signOut } from "next-auth/react";
 import Link from "next/link";
-
-import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function PrimaryButton({
 	label,
 	link,
 	className,
+	onClick,
 }: {
 	label: string;
-	link: string;
+	link?: string;
 	className?: string;
+	onClick?: () => void;
 }) {
-	const { data: session } = useSession();
-	let onClick = () => {};
-	if (label === "Login") {
-		if (session) {
-			label = "Logout";
-			onClick = () => signOut();
-		} else {
-			onClick = () => signIn();
+	if (link) {
+		return (
+			<Link
+				className={
+					"bg-primary py-3 px-5 rounded text-xl font-bold hover:-translate-y-1 hover:shadow-secondary " +
+					className
+				}
+				href={link}
+			>
+				{label}
+			</Link>
+		);
+	} else {
+		if (label === "Login" || label === "Logout") {
+			onClick = label == "Login" ? signIn : signOut;
 		}
+		return (
+			<button
+				className={
+					"bg-primary py-3 px-5 rounded text-xl font-bold hover:-translate-y-1 hover:shadow-secondary " +
+					className
+				}
+				onClick={onClick}
+			>
+				{label}
+			</button>
+		);
 	}
-
-	return (
-		<Link
-			className={
-				"bg-primary py-3 px-5 rounded text-xl font-bold hover:-translate-y-1 hover:shadow-secondary " +
-				className
-			}
-			href={link}
-			onClick={onClick}
-		>
-			{label}
-		</Link>
-	);
 }
