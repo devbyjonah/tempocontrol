9914634913d3f0466561771a7656b37694a973fa
jonah wagner <jonahwagner231@gmail.com>
2023-06-24 23:14:08 -0700

    major refactor for modalContent to make component more reusable and reduce repetition

--- app/metronome/metronome.tsx
+++ app/metronome/metronome.tsx
@@ -4,133 +4,143 @@ import MetronomeEngine from "./metronomeEngine";
 import Button from "@/components/button";
 import Slider from "@/components/slider";
 import SettingsModal from "./settingsModal";
-import { SubdivisionModal, TimeSignatureModal } from "./modalContent";
+import ModalContent from "./modalContent";
 
 import { useRef, useState, useEffect } from "react";
 
 export default function Metronome() {
-  // animation callback is passed to the metronome engine and invoked on each beat
-  const animationCallback = (beat: number, secondsPerBeat: number) => {
-    const beater: HTMLImageElement | null = document.querySelector("#beater");
-    if (beater) {
-      const rotateValue =
-        beater.style.transform === "translate(-50%) rotate(30deg)"
-          ? "-30deg"
-          : "30deg";
+    // animation callback is passed to the metronome engine and invoked on each beat
+    const animationCallback = (beat: number, secondsPerBeat: number) => {
+        const beater: HTMLImageElement | null =
+            document.querySelector("#beater");
+        if (beater) {
+            const rotateValue =
+                beater.style.transform === "translate(-50%) rotate(30deg)"
+                    ? "-30deg"
+                    : "30deg";
 
-      beater.style.transition = `transform ${secondsPerBeat}s linear`;
-      beater.style.transform = `translate(-50%) rotate(${rotateValue})`;
-    }
-  };
-  // store instance of MetronomeEngine in a ref so that it persists between renders
-  // the ref acts as the single source of truth for the metronome state
-  const metronomeEngine = useRef(new MetronomeEngine(animationCallback));
-  // state for metronome values that are set by user
-  const [tempo, setTempo] = useState(metronomeEngine.current.tempo);
-  const [playing, setPlaying] = useState(metronomeEngine.current.playing);
-  const [subdivision, setSubdivision] = useState(
-    metronomeEngine.current.subdivision
-  );
-  const [beatsPerMeasure, setBeatsPerMeasure] = useState(
-    metronomeEngine.current.beatsPerMeasure
-  );
-
-  // state for managing settings modals
-  const [modalOpen, setModalOpen] = useState(false);
-  const [modalContent, setModalContent] = useState(<>Default Modal</>);
-
-  // event handlers for metronome
-  const startStop = () => {
-    metronomeEngine.current.startStop();
-    setPlaying(metronomeEngine.current.playing);
-    const beater: HTMLImageElement | null = document.querySelector("#beater");
-    beater!!.style.transform = "translate(-50%) rotate(0deg)";
-  };
-  const changeSubdivision = (value: number): number => {
-    metronomeEngine.current.subdivision = value;
-    setSubdivision(metronomeEngine.current.subdivision);
-    return metronomeEngine.current.subdivision;
-  };
-  const changeTimeSignature = (value: number): number => {
-    metronomeEngine.current.beatsPerMeasure = value;
-    setBeatsPerMeasure(metronomeEngine.current.beatsPerMeasure);
-    return metronomeEngine.current.beatsPerMeasure;
-  };
-  // event handlers to open and set modal content
-  const openSubdivideModal = (): void => {
-    setModalContent(
-      <SubdivisionModal
-        initialValue={subdivision}
-        changeSubdivision={changeSubdivision}
-      />
-    );
-    setModalOpen(true);
-  };
-  const openTimeSignatureModal = (): void => {
-    setModalContent(
-      <TimeSignatureModal
-        initialValue={beatsPerMeasure}
-        changeTimeSignature={changeTimeSignature}
-      />
-    );
-    setModalOpen(true);
-  };
-
-  useEffect(() => {
-    // Cleanup function to stop the metronome and remove the audio context
-    return () => {
-      metronomeEngine.current.cleanup();
+            beater.style.transition = `transform ${secondsPerBeat}s linear`;
+            beater.style.transform = `translate(-50%) rotate(${rotateValue})`;
+        }
     };
-  }, []);
+    // store instance of MetronomeEngine in a ref so that it persists between renders
+    // the ref acts as the single source of truth for the metronome state
+    const metronomeEngine = useRef(new MetronomeEngine(animationCallback));
+    // state for metronome values that are set by user
+    const [tempo, setTempo] = useState(metronomeEngine.current.tempo);
+    const [playing, setPlaying] = useState(metronomeEngine.current.playing);
+    const [subdivision, setSubdivision] = useState(
+        metronomeEngine.current.subdivision
+    );
+    const [beatsPerMeasure, setBeatsPerMeasure] = useState(
+        metronomeEngine.current.beatsPerMeasure
+    );
 
-  return (
-    <div className="flex flex-col-reverse sm:flex-row">
-      <SettingsModal
-        isOpen={modalOpen}
-        onClose={() => setModalOpen(false)}
-        content={modalContent}
-      />
-      <div
-        className="relative mt-28 sm:mt-40 mx-auto"
-        style={{ maxWidth: 500 + "px", maxHeight: 750 + "px" }}
-      >
-        <Image
-          id="base"
-          src="/metronomeBase.png"
-          alt="metronome"
-          width={500}
-          height={750}
-          priority
-        />
-        <Image
-          id="beater"
-          className="absolute h-full -top-2 left-1/2 origin-bottom -translate-x-1/2"
-          src="/beater.png"
-          alt="metronome"
-          width={50}
-          height={750}
-        />
-      </div>
-      <div className="sm:w-2/5 text-black flex flex-col items-center sm:items-stretch">
-        <div className="max-w-xs rounded settings-container my-auto flex flex-col gap-2">
-          <Button
-            label={playing ? "Stop" : "Start"}
-            onClick={startStop}
-            className="bg-primary"
-          />
-          <Button
-            className="bg-primary"
-            label="Subdivide"
-            onClick={openSubdivideModal}
-          />
-          <Button
-            className="bg-primary"
-            label="Time Signature"
-            onClick={openTimeSignatureModal}
-          />
+    // state for managing settings modals
+    const [modalOpen, setModalOpen] = useState(false);
+    const [modalContent, setModalContent] = useState(<>Default Modal</>);
+
+    // event handlers for metronome
+    const startStop = () => {
+        metronomeEngine.current.startStop();
+        setPlaying(metronomeEngine.current.playing);
+        const beater: HTMLImageElement | null =
+            document.querySelector("#beater");
+        beater!!.style.transform = "translate(-50%) rotate(0deg)";
+    };
+    const changeSubdivision = (value: number): number => {
+        metronomeEngine.current.subdivision = value;
+        setSubdivision(metronomeEngine.current.subdivision);
+        return metronomeEngine.current.subdivision;
+    };
+    const changeTimeSignature = (value: number): number => {
+        metronomeEngine.current.beatsPerMeasure = value;
+        setBeatsPerMeasure(metronomeEngine.current.beatsPerMeasure);
+        return metronomeEngine.current.beatsPerMeasure;
+    };
+    // event handlers to open and set modal content
+    const openSubdivideModal = (): void => {
+        setModalContent(
+            <ModalContent
+                title="Subdivide"
+                description="Select the number of subdivisions per beat."
+                hint="Combine with time signature for complex divisions"
+                initialValue={subdivision}
+                changeValue={changeSubdivision}
+                numberOptions={[1, 2, 3, 4]}
+            />
+        );
+        setModalOpen(true);
+    };
+    const openTimeSignatureModal = (): void => {
+        setModalContent(
+            <ModalContent
+                title="Time Signature"
+                description="Select the number of beats per measure."
+                hint="Use the arrows to select a number not listed below."
+                initialValue={beatsPerMeasure}
+                changeValue={changeTimeSignature}
+                numberOptions={[1, 2, 3, 4, 5, 6]}
+            />
+        );
+        setModalOpen(true);
+    };
+
+    useEffect(() => {
+        // Cleanup function to stop the metronome and remove the audio context
+        return () => {
+            metronomeEngine.current.cleanup();
+        };
+    }, []);
+
+    return (
+        <div className="flex flex-col-reverse sm:flex-row">
+            <SettingsModal
+                isOpen={modalOpen}
+                onClose={() => setModalOpen(false)}
+                content={modalContent}
+            />
+            <div
+                className="relative mt-28 sm:mt-40 mx-auto"
+                style={{ maxWidth: 500 + "px", maxHeight: 750 + "px" }}
+            >
+                <Image
+                    id="base"
+                    src="/metronomeBase.png"
+                    alt="metronome"
+                    width={500}
+                    height={750}
+                    priority
+                />
+                <Image
+                    id="beater"
+                    className="absolute h-full -top-2 left-1/2 origin-bottom -translate-x-1/2"
+                    src="/beater.png"
+                    alt="metronome"
+                    width={50}
+                    height={750}
+                />
+            </div>
+            <div className="sm:w-2/5 text-black flex flex-col items-center sm:items-stretch">
+                <div className="max-w-xs rounded settings-container my-auto flex flex-col gap-2">
+                    <Button
+                        label={playing ? "Stop" : "Start"}
+                        onClick={startStop}
+                        className="bg-primary"
+                    />
+                    <Button
+                        className="bg-primary"
+                        label="Subdivide"
+                        onClick={openSubdivideModal}
+                    />
+                    <Button
+                        className="bg-primary"
+                        label="Time Signature"
+                        onClick={openTimeSignatureModal}
+                    />
+                </div>
+                <Slider />
+            </div>
         </div>
-        <Slider />
-      </div>
-    </div>
-  );
+    );
 }
--- app/metronome/modalContent.tsx
+++ app/metronome/modalContent.tsx
@@ -3,116 +3,63 @@ import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
 
 import { useState } from "react";
 
-const OButton = ({
-	label,
-	onClick,
-}: {
-	label: string;
-	onClick: () => void;
-}) => {
-	return (
-		<Button
-			onClick={onClick}
-			label={label}
-			className="bg-primary rounded-full"
-		/>
-	);
-};
-
-export const SubdivisionModal = ({
-	changeSubdivision,
+const ModalContent = ({
+	title,
+	description,
+	hint,
+	changeValue,
 	initialValue,
+	numberOptions,
 }: {
-	changeSubdivision: (value: number) => number;
+	title: string;
+	description: string;
+	hint: string;
+	changeValue: (value: number) => number;
 	initialValue: number;
+	numberOptions: number[];
 }) => {
 	const [value, setValue] = useState(initialValue);
-	const onClick = (value: number) => {
-		const updatedSubdivision = changeSubdivision(value);
-		setValue(updatedSubdivision);
+	const updateValue = (value: number) => {
+		const newValue = changeValue(value);
+		setValue(newValue);
 	};
 
 	return (
 		<div className="bg-white rounded-full w-full h-full p-5 flex flex-col gap-6">
 			<div className="text-center pt-4">
-				<h1 className="text-2xl font-bold">Subdivision</h1>
-				<p className="text-accent">How many subdivisions per beat?</p>
-				<p className="text-accent">
-					Combine with time signature for complex subdivisions.
-				</p>
+				<h1 className="text-2xl font-bold">{title}</h1>
+				<p className="text-accent">{description}</p>
+				<p className="text-accent">{hint}</p>
 			</div>
 			<div className="bg-primary w-1/3 mx-auto rounded-full flex justify-center items-center gap-5 text-2xl">
 				<BsArrowLeftCircleFill
 					onClick={() => {
-						onClick(value - 1);
+						updateValue(value - 1);
 					}}
 					className="hover:scale-150"
 				/>
 				<h1 className="text-5xl font-bold">{value}</h1>
 				<BsArrowRightCircleFill
 					onClick={() => {
-						onClick(value + 1);
+						updateValue(value + 1);
 					}}
 					className="hover:scale-150"
 				/>
 			</div>
 			<div className="flex justify-around">
-				<OButton onClick={() => onClick(1)} label="1" />
-				<OButton onClick={() => onClick(2)} label="2" />
-				<OButton onClick={() => onClick(3)} label="3" />
-				<OButton onClick={() => onClick(4)} label="4" />
+				{numberOptions.map((option) => (
+					<Button
+						key={option}
+						label={String(option)}
+						onClick={() => updateValue(option)}
+						className={
+							option === value ? "bg-accent" : "bg-primary"
+						}
+					/>
+				))}
 			</div>
 		</div>
 	);
 };
 
-export const TimeSignatureModal = ({
-	initialValue,
-	changeTimeSignature,
-}: {
-	initialValue: number;
-	changeTimeSignature: (value: number) => number;
-}) => {
-	const [beatsPerMeasure, setBeatsPerMeasure] = useState(initialValue);
-	const onClick = (value: number) => {
-		const updatedBeatsPerMeasure = changeTimeSignature(value);
-		setBeatsPerMeasure(updatedBeatsPerMeasure);
-	};
-
-	return (
-		<div className="bg-white rounded-full w-full h-full p-5 flex flex-col gap-6">
-			<div className="text-center pt-4">
-				<h1 className="text-2xl font-bold">Time Signature</h1>
-				<p className="text-accent">How many beats per measure?</p>
-				<p className="text-accent">
-					Use the arrows to set a number not listed below.
-				</p>
-			</div>
-			<div className="bg-primary w-1/3 mx-auto rounded-full flex justify-center items-center gap-5 text-2xl">
-				<BsArrowLeftCircleFill
-					onClick={() => {
-						onClick(beatsPerMeasure - 1);
-					}}
-					className="hover:scale-150"
-				/>
-				<h1 className="text-5xl font-bold">{beatsPerMeasure}</h1>
-				<BsArrowRightCircleFill
-					onClick={() => {
-						onClick(beatsPerMeasure + 1);
-					}}
-					className="hover:scale-150"
-				/>
-			</div>
-			<div className="flex justify-around">
-				<OButton label="1" onClick={() => onClick(1)} />
-				<OButton onClick={() => onClick(2)} label="2" />
-				<OButton onClick={() => onClick(3)} label="3" />
-				<OButton onClick={() => onClick(4)} label="4" />
-			</div>
-			<div className="flex justify-center gap-8">
-				<OButton onClick={() => onClick(5)} label="5" />
-				<OButton onClick={() => onClick(6)} label="6" />
-			</div>
-		</div>
-	);
-};
+export default ModalContent;
