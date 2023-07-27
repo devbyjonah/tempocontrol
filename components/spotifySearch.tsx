e17ce70f3db0e02b3bb589d71cb9f6839d6f9415
jonah wagner <jonahwagner231@gmail.com>
2023-07-26 17:36:51 -0700

    finished first iteration of spotify search bar

--- components/spotifySearch.tsx
+++ components/spotifySearch.tsx
@@ -1,15 +1,20 @@
 "use client";
-import { useEffect, useState } from "react";
-import Image from "next/image";
+import { useEffect, useState, useRef } from "react";
+
 import { getSpotifyToken, searchSpotify } from "../lib/spotify";
 import SpotifyTrack from "@/interfaces/spotify";
 import TrackPreview from "./trackPreview";
 
 export default function SpotifySearch() {
-	const [token, setToken] = useState(null);
+	const [token, setToken] = useState("");
 	const [searchResults, setSearchResults] = useState<JSX.Element[]>([]);
+	const debounceRef = useRef<NodeJS.Timeout>();
 
 	async function search(token: string, query: string) {
+		if (!query.trim()) {
+			setSearchResults([]);
+			return;
+		}
 		const tracks = await searchSpotify(token, query);
 		if (tracks) {
 			const previewElements: JSX.Element[] = tracks.map(
@@ -21,24 +26,36 @@ export default function SpotifySearch() {
 		}
 	}
 
+	function debounceSearch(query: string) {
+		if (debounceRef.current) {
+			clearTimeout(debounceRef.current);
+		}
+		debounceRef.current = setTimeout(() => search(token, query), 300);
+	}
+
 	useEffect(() => {
 		async function fetchToken() {
 			const token = await getSpotifyToken();
 			setToken(token);
 		}
 		fetchToken();
+		return () => {
+			if (debounceRef.current) {
+				clearTimeout(debounceRef.current);
+			}
+		};
 	}, []);
 
 	return (
-		<div
-			className="flex flex-col gap-4"
-			onClick={() => {
-				if (token) {
-					search(token, "The Beatles");
-				}
-			}}
-		>
-			<h1 className="text-lg text-white">Select BPM of a song</h1>
+		<div className="flex flex-col gap-4">
+			<input
+				type="text"
+				placeholder="Search songs"
+				onChange={(value) => debounceSearch(value.target.value)}
+			/>
+			<h1 className="text-lg text-white">
+				Search and select song to set BPM.
+			</h1>
 			{searchResults}
 		</div>
 	);
--- components/trackPreview.tsx
+++ components/trackPreview.tsx
@@ -3,7 +3,6 @@ import Image from "next/image";
 import { Key } from "react";
 
 export default function TrackPreview({ track }: { track: SpotifyTrack }) {
-	console.log(track);
 	return (
 		<div id={track.id} className="w-full rounded flex p-4 bg-white gap-4">
 			<Image
